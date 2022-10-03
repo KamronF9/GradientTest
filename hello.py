499681999616fb28580ee6dd387aa8ecee511b3b
KamronF9 <78815657+KamronF9@users.noreply.github.com>
2022-10-03 15:41:09 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+#save this file as hello.py in your repo
+import tensorflow as tf
+
+# Simple hello world using TensorFlow
+hello = 'Hello, TensorFlow!'
+
+# Start tf session
+#sess = tf.Session()
+
+# Run the op
+#print(sess.run(hello))
+fpOut = open('test.txt',"w")
+print(hello,file=fpOut)
+fpOut.close() 
